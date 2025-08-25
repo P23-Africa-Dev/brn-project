2eab3bc10881deb51f231bd5a9f161a630dc0efb
Nurudeen O. Daniju <59366854+Dnurudeen@users.noreply.github.com>
2025-08-25 18:25:55 +0100

    error page ui (#1)
    * working on setup
    
    * completed setup
    
    * completed error-page
    
    ---------
    
    Co-authored-by: Rasheed124 <rashdevatrealglobe@gmail.com>

---
+++ resources/js/constants/image.ts
@@ -0,0 +1,25 @@
+// src/constants/images.ts
+// import stepFormsBg from "../assets/stepForms-bg.png";
+// import stepFormsPattern from "../assets/stepForms-pattern.png";
+// import stepFormsInnerPattern from "../assets/stepForms-inner-bg.png";
+// import uploadBg from "../assets/upload.png";
+// import badge from "../assets/badge.png";
+// import badgeMark from "../assets/badge-mark.png";
+// import spinner from "../assets/spinerr.png";
+// import formBG from "../assets/formBg.png";
+
+import errorBg from "../../../public/assets/error-bg.png";
+// Export all images in an object
+const images = {
+//   uploadBg,
+//   stepFormsBg,
+//   badge,
+//   badgeMark,
+//   spinner,
+//   stepFormsPattern,
+//   formBG,
+//   stepFormsInnerPattern
+  errorBg
+};
+
+export default images;
