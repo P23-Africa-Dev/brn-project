4987ddebc90a149b04291407ade0b02027f1c859
Rasheed124 <rashdevatrealglobe@gmail.com>
2025-08-25 17:17:59 +0100

    completed error-page

--- resources/js/pages/auth/error/auth-error-page.tsx
+++ resources/js/pages/auth/error/auth-error-page.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button-old';
+import images from '@/constants/image';
 
 interface Props {
     message: string;
@@ -12,12 +13,15 @@ export default function AuthErrorPage({ message, buttonText, buttonLink }: Props
     };
 
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center p-4">
-            <div className="space-y-6 text-center">
-                <h1 className="text-2xl font-semibold text-red-600">Error</h1>
-                <p className="text-gray-600">{message}</p>
-                <Button onClick={handleClick} className="cursor-pointer px-6 py-2 hover:cursor-pointer">
-                    {buttonText}
+        <div className="flex min-h-screen flex-col items-center justify-center bg-white p-4">
+            <div className="max-w-md space-y-4 text-center">
+                <div className='w-full flex justify-center items-center'>
+                    <img src={images.errorBg} alt="" />
+                </div>
+                <h1 className="text-2xl  text-[#0E0842] font-bold">Opps!</h1>
+                <p className="text-[#5E4F85]">Complete our 30-second form for exclusive, tailored access to our executive networking platform.</p>
+                <Button onClick={handleClick} className="cursor-pointer bg-[#1D1D1D] px-6 py-2 text-white hover:cursor-pointer hover:bg-[#1D1D1D]/90">
+                    Proceed to show interest
                 </Button>
             </div>
         </div>
